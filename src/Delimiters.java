d6862390c1e0dedf157348f0645b47c4afaa6619
kenny_xue3 <kennyx3@nycstudents.net>
2022-02-14 13:33:27 -0500

    Finished

--- src/Delimiters.java
+++ src/Delimiters.java
@@ -1,32 +1,53 @@
 import java.util.ArrayList;
 
-public class Delimiters
-{
-    /** The open and close delimiters **/
+public class Delimiters {
+    /**
+     * The open and close delimiters
+     **/
     private String openDel;
     private String closeDel;
 
-    /** Constructs a Delimiters object where open is the open delimiter and close is the
-     *  close delimiter.
-     *  Precondition: open and close are non-empty strings.
+    /**
+     * Constructs a Delimiters object where open is the open delimiter and close is the
+     * close delimiter.
+     * Precondition: open and close are non-empty strings.
      */
-    public Delimiters(String open, String close)
-    {
+    public Delimiters(String open, String close) {
         openDel = open;
         closeDel = close;
     }
 
-    /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
-    public ArrayList<String> getDelimitersList(String[] tokens)
-    {
-        /* to be implemented in part (a) */
+    /**
+     * Returns an ArrayList of delimiters from the array tokens, as described in part (a).
+     */
+    public ArrayList<String> getDelimitersList(String[] tokens) {
+        ArrayList<String> delimiterList = new ArrayList<>();
+        for (String string : tokens) {
+            if (string.equals(openDel) || string.equals(closeDel)) {
+                delimiterList.add(string);
+            }
+        }
+        return delimiterList;
     }
 
-    /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
-     *  Precondition: delimiters contains only valid open and close delimiters.
+    /**
+     * Returns true if the delimiters are balanced and false otherwise, as described in part (b).
+     * Precondition: delimiters contains only valid open and close delimiters.
      */
-    public boolean isBalanced(ArrayList<String> delimiters)
-    {
-        /* to be implemented in part (b) */
+    public boolean isBalanced(ArrayList<String> delimiters) {
+        int openCount = 0;
+        int closeCount = 0;
+        for (String delimiter : delimiters) {
+            if (delimiter.equals(openDel)) {
+                openCount++;
+            }
+            if (delimiter.equals(closeDel)) {
+                closeCount++;
+                if (closeCount > openCount) {
+                    return false;
+                }
+            }
+        }
+        return openCount == closeCount;
     }
 }
